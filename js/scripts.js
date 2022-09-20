b6de22393cdbada70b145985e138c3af607d304f
Marcin <edenus27@gmail.com>
2022-09-20 21:25:41 +0200

    added tittle and text , create new section

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-const mobilNav = document.querySelector(".mobil-nav")
-const menu = document.querySelector(".menu")
-
+const mobilNav = document.querySelector(".mobil-nav");
+const menu = document.querySelector(".menu");
 
 const showMobileMenu = () => {
-    menu.classList.toggle("menu-active")
-    mobilNav.classList.toggle(".mobil-nav-active")
-}
-mobilNav.addEventListener("click", showMobileMenu)
+	menu.classList.toggle("hide");
+	mobilNav.classList.toggle(".mobil-nav-active");
+};
+mobilNav.addEventListener("click", showMobileMenu);
